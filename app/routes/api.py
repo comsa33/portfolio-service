cfa1681c254613f6d5e757affb3c7aeaf2c4ce60
ruo <ruo@gravylab.co.kr>
2023-09-08 13:17:39 +0900

    fix: api

--- app/routes/api.py
+++ app/routes/api.py
@@ -16,15 +16,20 @@ def get_basic_info():
 @api.route('/basic_info', methods=['POST'])
 def update_basic_info():
     data = request.get_json()
+    print("Received data:", data)  # 로그에 수신된 데이터 출력
+
     basic_info = BasicInfo.query.first()
-
-    if not basic_info:
-        # Create new record if it doesn't exist
-        basic_info = BasicInfo(**data)
-        db.session.add(basic_info)
+    if basic_info:
+        basic_info.update(data)  # 기존 객체 업데이트
     else:
-        # Update existing record
-        basic_info.update(data)
+        new_info = BasicInfo(**data)  # 새 객체 생성
+        db.session.add(new_info)
 
-    db.session.commit()
-    return jsonify(basic_info.to_dict())
+    try:
+        db.session.commit()
+    except Exception as e:
+        print("Database error:", e)
+        db.session.rollback()
+        return jsonify({"error": "Database error"}), 500
+
+    return jsonify(data)
