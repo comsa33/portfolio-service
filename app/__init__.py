fbc60f85a975a80b251ba25243db2045b6f540a8
ruo <ruo@gravylab.co.kr>
2023-09-08 11:04:50 +0900

    add: flask_migrate

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 db = SQLAlchemy()
+migrate = Migrate()
 
 
 def create_app():
@@ -9,6 +11,8 @@ def create_app():
     app.config.from_object('config')
 
     db.init_app(app)
+    migrate.init_app(app, db)
+
     with app.app_context():
         db.create_all()
 
