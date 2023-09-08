6690201127f4b715c43ab31082adfc82b3180523
ruo <ruo@gravylab.co.kr>
2023-09-08 13:51:13 +0900

    add: additional_info html js api

--- app/routes/api.py
+++ app/routes/api.py
@@ -33,3 +33,99 @@ def update_basic_info():
         return jsonify({"error": "Database error"}), 500
 
     return jsonify(data)
+
+
+@api.route('/career', methods=['POST'])
+def create_career():
+    data = request.get_json()
+    new_career = Career(
+        basic_info_id=data['basic_info_id'],
+        company_name_eng=data['company_name_eng'],
+        company_name_kor=data['company_name_kor'],
+        company_type_eng=data['company_type_eng'],
+        company_type_kor=data['company_type_kor'],
+        department_eng=data['department_eng'],
+        department_kor=data['department_kor'],
+        position_eng=data['position_eng'],
+        position_kor=data['position_kor'],
+        start_date=data['start_date'],
+        end_date=data['end_date'],
+        description_eng=data['description_eng'],
+        description_kor=data['description_kor']
+    )
+    db.session.add(new_career)
+    db.session.commit()
+    return jsonify(new_career.to_dict()), 201
+
+
+@api.route('/project', methods=['POST'])
+def create_project():
+    data = request.get_json()
+    new_project = Project(
+        basic_info_id=data['basic_info_id'],
+        project_name_eng=data['project_name_eng'],
+        project_name_kor=data['project_name_kor'],
+        project_main_type_eng=data['project_main_type_eng'],
+        project_main_type_kor=data['project_main_type_kor'],
+        project_sub_type_eng=data['project_sub_type_eng'],
+        project_sub_type_kor=data['project_sub_type_kor'],
+        start_date=data['start_date'],
+        end_date=data['end_date'],
+        no_of_team_members=data['no_of_team_members'],
+        team_name_eng=data['team_name_eng'],
+        team_name_kor=data['team_name_kor'],
+        summary_eng=data['summary_eng'],
+        summary_kor=data['summary_kor'],
+        role_description_eng=data['role_description_eng'],
+        role_description_kor=data['role_description_kor'],
+        issue_description_eng=data['issue_description_eng'],
+        issue_description_kor=data['issue_description_kor'],
+        project_link=data['project_link'],
+        code_link=data['code_link'],
+        project_image=data['project_image']
+    )
+    db.session.add(new_project)
+    db.session.commit()
+    return jsonify(new_project.to_dict()), 201
+
+
+@api.route('/skill', methods=['POST'])
+def create_skill():
+    data = request.get_json()
+    new_skill = Skill(
+        basic_info_id=data['basic_info_id'],
+        skill_name_eng=data['skill_name_eng'],
+        skill_name_kor=data['skill_name_kor'],
+        skill_type_eng=data['skill_type_eng'],
+        skill_type_kor=data['skill_type_kor'],
+        start_date=data['start_date'],
+        skill_level=data['skill_level'],
+        description_eng=data['description_eng'],
+        description_kor=data['description_kor'],
+        skill_image=data['skill_image']
+    )
+    db.session.add(new_skill)
+    db.session.commit()
+    return jsonify(new_skill.to_dict()), 201
+
+
+@api.route('/education', methods=['POST'])
+def create_education():
+    data = request.get_json()
+    new_education = Education(
+        basic_info_id=data['basic_info_id'],
+        school_name_eng=data['school_name_eng'],
+        school_name_kor=data['school_name_kor'],
+        degree_eng=data['degree_eng'],
+        degree_kor=data['degree_kor'],
+        major_eng=data['major_eng'],
+        major_kor=data['major_kor'],
+        start_date=data['start_date'],
+        end_date=data['end_date'],
+        description_eng=data['description_eng'],
+        description_kor=data['description_kor'],
+        logo_image=data['logo_image']
+    )
+    db.session.add(new_education)
+    db.session.commit()
+    return jsonify(new_education.to_dict()), 201
---
+++ static/js/additional_info.js
@@ -0,0 +1,152 @@
+document.addEventListener("DOMContentLoaded", function() {
+    // Fetch basic_info IDs and populate the dropdown
+    fetch("/api/basic_info")
+    .then(response => response.json())
+    .then(data => {
+        const basicInfoDropdown = document.getElementById("career-basic-info-id");
+        data.forEach(basicInfo => {
+            const option = document.createElement("option");
+            option.value = basicInfo.id;
+            option.textContent = basicInfo.first_name_eng + " " + basicInfo.last_name_eng;
+            basicInfoDropdown.appendChild(option);
+        });
+    });
+    
+    // Your code to handle form submission and send the data to the server
+    document.getElementById("submit-career").addEventListener("click", function() {
+        const careerData = {
+            basic_info_id: document.getElementById("career-basic-info-id").value,  // 드롭다운에서 선택
+            company_name_eng: document.getElementById("company_name_eng").value,
+            company_name_kor: document.getElementById("company_name_kor").value,
+            company_type_eng: document.getElementById("company_type_eng").value,
+            company_type_kor: document.getElementById("company_type_kor").value,
+            department_eng: document.getElementById("department_eng").value,
+            department_kor: document.getElementById("department_kor").value,
+            position_eng: document.getElementById("position_eng").value,
+            position_kor: document.getElementById("position_kor").value,
+            start_date: document.getElementById("career-start-date").value,
+            end_date: document.getElementById("career-end-date").value,
+            description_eng: document.getElementById("career-description-eng").value,
+            description_kor: document.getElementById("career-description-kor").value
+        };
+    
+        fetch("/api/career", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(careerData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Career data saved:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    });
+
+    document.getElementById("submit-project").addEventListener("click", function() {
+        const projectData = {
+            basic_info_id: document.getElementById("project-basic-info-id").value,  // 드롭다운에서 선택
+            project_name_eng: document.getElementById("project_name_eng").value,
+            project_name_kor: document.getElementById("project_name_kor").value,
+            project_main_type_eng: document.getElementById("project_main_type_eng").value,
+            project_main_type_kor: document.getElementById("project_main_type_kor").value,
+            project_sub_type_eng: document.getElementById("project_sub_type_eng").value,
+            project_sub_type_kor: document.getElementById("project_sub_type_kor").value,
+            start_date: document.getElementById("project-start-date").value,
+            end_date: document.getElementById("project-end-date").value,
+            no_of_team_members: document.getElementById("no_of_team_members").value,
+            team_name_eng: document.getElementById("team_name_eng").value,
+            team_name_kor: document.getElementById("team_name_kor").value,
+            summary_eng: document.getElementById("summary_eng").value,
+            summary_kor: document.getElementById("summary_kor").value,
+            role_description_eng: document.getElementById("role_description_eng").value,
+            role_description_kor: document.getElementById("role_description_kor").value,
+            issue_description_eng: document.getElementById("issue_description_eng").value,
+            issue_description_kor: document.getElementById("issue_description_kor").value,
+            project_link: document.getElementById("project_link").value,
+            code_link: document.getElementById("code_link").value,
+            project_image: document.getElementById("project_image").value
+        };
+    
+        fetch("/api/project", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(projectData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Project data saved:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    });
+
+    document.getElementById("submit-skill").addEventListener("click", function() {
+        const skillData = {
+            basic_info_id: document.getElementById("skill-basic-info-id").value,  // 드롭다운에서 선택
+            skill_name_eng: document.getElementById("skill_name_eng").value,
+            skill_name_kor: document.getElementById("skill_name_kor").value,
+            skill_type_eng: document.getElementById("skill_type_eng").value,
+            skill_type_kor: document.getElementById("skill_type_kor").value,
+            start_date: document.getElementById("skill-start-date").value,
+            skill_level: document.getElementById("skill-level").value,
+            description_eng: document.getElementById("description_eng").value,
+            description_kor: document.getElementById("description_kor").value,
+            skill_image: document.getElementById("skill_image").value
+        };
+    
+        fetch("/api/skill", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(skillData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Skill data saved:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    });    
+
+    document.getElementById("submit-education").addEventListener("click", function() {
+        const educationData = {
+            basic_info_id: document.getElementById("education-basic-info-id").value,
+            school_name_eng: document.getElementById("school_name_eng").value,
+            school_name_kor: document.getElementById("school_name_kor").value,
+            degree_eng: document.getElementById("degree_eng").value,
+            degree_kor: document.getElementById("degree_kor").value,
+            major_eng: document.getElementById("major_eng").value,
+            major_kor: document.getElementById("major_kor").value,
+            start_date: document.getElementById("education-start-date").value,
+            end_date: document.getElementById("education-end-date").value,
+            description_eng: document.getElementById("education-description-eng").value,
+            description_kor: document.getElementById("education-description-kor").value,
+            logo_image: document.getElementById("logo_image").value
+        };
+    
+        fetch("/api/education", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(educationData)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log("Education data saved:", data);
+        })
+        .catch((error) => {
+            console.error("Error:", error);
+        });
+    });
+});
+
