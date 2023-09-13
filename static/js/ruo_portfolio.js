11320cdc2fc17cdd026c0e09206870254ef14b57
ruo <ruo@gravylab.co.kr>
2023-09-13 14:48:34 +0900

    fix: ruo_portfolio html js

--- static/js/ruo_portfolio.js
+++ static/js/ruo_portfolio.js
@@ -1,10 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
     let isEnglish = true;
 
-    // Toggle text function
-    const toggleText = (selector, attrEng, attrKor) => {
+    // Toggle text function without Markdown
+    const toggleTextWithoutMarkdown = (selector, attrEng, attrKor) => {
         document.querySelectorAll(selector).forEach(el => {
             el.innerHTML = isEnglish ? el.getAttribute(attrEng) : el.getAttribute(attrKor);
+        });
+    };
+
+    // Toggle text function with Markdown
+    const toggleTextWithMarkdown = (selector, attrEng, attrKor) => {
+        document.querySelectorAll(selector).forEach(el => {
+            const text = isEnglish ? el.getAttribute(attrEng) : el.getAttribute(attrKor);
+            el.innerHTML = marked(text);
         });
     };
 
@@ -16,17 +24,19 @@ document.addEventListener("DOMContentLoaded", function() {
         isEnglish = !isEnglish;
         document.getElementById('labelLanguage').textContent = isEnglish ? 'English' : '한글';
 
-        // Toggle text for all elements
-        toggleText('#fullName', 'data-eng', 'data-kor');
-        toggleText('#jobTitle', 'data-eng', 'data-kor');
-        toggleText('#address', 'data-eng', 'data-kor');
-        toggleText('#introduction', 'data-eng', 'data-kor');
-        toggleText('#educationTitle', 'data-eng', 'data-kor');
-        toggleText('#educationList li', 'data-eng', 'data-kor');
-        toggleText('#skillTitle', 'data-eng', 'data-kor');
-        toggleText('#skillList li', 'data-eng', 'data-kor');
-        toggleText('#careerTitle', 'data-eng', 'data-kor');
-        toggleText('#careerList .career-item h3', 'data-eng', 'data-kor');
-        toggleText('#careerList .project-item h4', 'data-eng', 'data-kor');
+        // Toggle text for all elements without Markdown
+        toggleTextWithoutMarkdown('#fullName', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#jobTitle', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#address', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#educationTitle', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#educationList li', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#skillTitle', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#skillList li', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#careerTitle', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#careerList .career-item h3', 'data-eng', 'data-kor');
+        toggleTextWithoutMarkdown('#careerList .project-item h4', 'data-eng', 'data-kor');
+
+        // Toggle text for Markdown elements
+        toggleTextWithMarkdown('#introduction', 'data-eng', 'data-kor');
     });
 });
